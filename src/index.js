5855fa6b25896c1f9768c25abfd8c66b0f97a85a
Matt Miller <mattmillerart@gmail.com>
2016-08-25 09:38:50 -0400

    Organized components into folders

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
+// deps
 import angular from 'angular';
-
-import {hello} from './app/hello';
-import {photoViewer} from './app/photo-viewer';
-import {icon} from './app/icon';
-import {homeButton} from './app/home-button';
-import {textReader} from './app/text-reader';
-import {back} from './app/back';
-import {category} from './app/category';
-import {titleBar} from './app/title-bar';
 import 'angular-ui-router';
-import routesConfig from './routes';
 import 'angular-sanitize';
 
+// components
+import {hello} from './app/hello/hello';
+import {photoViewer} from './app/photo-viewer/photo-viewer';
+import {icon} from './app/icon/icon';
+import {homeButton} from './app/home-button/home-button';
+import {textReader} from './app/text-reader/text-reader';
+import {back} from './app/back/back';
+import {category} from './app/category/category';
+import {titleBar} from './app/title-bar/title-bar';
+
+// routes
+import routesConfig from './routes';
+
+// styles
 import './index.scss';
 
+// export app
 export const app = 'app';
 
 angular
